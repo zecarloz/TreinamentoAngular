17b82b793949c38a641bb8329d162e5071ccb23b
Jose carlos de oliveira <josecarlos2503@hotmail.com>
2020-10-20 14:43:51 -0300

    adicionado masi arquivos

---
+++ OneDrive/Área de Trabalho/teste/teste.js
@@ -0,0 +1,6 @@
+"use strict";
+var meuNome = "José Carlos";
+function dizOla(nome) {
+    console.log("Olá " + nome); // saída será: Olá Thiago guedes
+}
+dizOla(meuNome);
---
+++ OneDrive/Área de Trabalho/teste/teste.ts
@@ -0,0 +1,5 @@
+let meuNome = "José Carlos";
+function dizOla(nome: string){
+console.log("Olá " + nome); // saída será: Olá Thiago guedes
+}
+dizOla(meuNome);
